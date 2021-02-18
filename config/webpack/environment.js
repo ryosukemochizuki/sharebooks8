069afba6a64b3aa09a5044cf7110490dd668bed7
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-02-18 23:33:49 +0900

    setup bootstrap and jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+// jQueryとpopper.jsをwebpackで全ての環境で利用できるようにする
+const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
