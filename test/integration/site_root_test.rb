60b4dff92bd15d08445e12d45f3bff131766b06a
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-02-19 17:12:02 +0900

    static_pages header footer

---
+++ test/integration/site_root_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SiteRootTest < ActionDispatch::IntegrationTest
+  
+  test "root_path links" do
+    get root_path
+    assert_select "a[href=?]", root_path, count: 3
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
