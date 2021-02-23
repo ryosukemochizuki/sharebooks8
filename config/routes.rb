9dad1b486722b3614d51ab02f8324206e7924b14
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-02-23 10:18:22 +0900

    rails g controller Sessions, setup routes, setup new.html.erb, small test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  # static_pages
   root 'static_pages#home'
   get 'about',   to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  # users
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  # sessions
+  get 'login',     to: 'sessions#new'
+  post 'login',    to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
